52997c591984ac509a68fe23babb75c40d790804
Yaroslav51-dev <ixityzv@gmail.com>
2024-07-28 22:28:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # Taking from AnymoreProject kernel source 
 #
 #
-sudo apt update && sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio
+sudo apt update && sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio tar gzip build-essential
 
 export KBUILD_BUILD_HOST=Yaroslav
 export KBUILD_BUILD_USER=t.me
@@ -12,9 +12,8 @@ export KBUILD_BUILD_USER=t.me
 # git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang.git -b master clang
     
 make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all)
-mkdir clang
-git clone -q --depth=1 https://github.com/XSans0/WeebX-Clang/releases/download/WeebX-Clang-18.1.8-release/WeebX-Clang-18.1.8.tar.gz /clang/
-tar -xvzf WeebX-Clang-18.1.8.tar.gz
+git clone -q --depth=1 https://github.com/XSans0/WeebX-Clang/releases/download/WeebX-Clang-18.1.8-release/WeebX-Clang-18.1.8.tar.gz $HOME/vayu/clang
+tar -xvzf $HOME/vayu/clang/WeebX-Clang-18.1.8.tar.gz
 export PATH="$HOME/vayu/clang/bin:$PATH"
 
 make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
