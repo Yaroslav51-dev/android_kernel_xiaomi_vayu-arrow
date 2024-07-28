5ee27d4a58d732b0e23fb21ffe944ec815453390
Yaroslav51-dev <ixityzv@gmail.com>
2024-07-28 20:24:16 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ sudo apt update && sudo -H apt-get install bc python2 ccache binutils-aarch64-li
 export KBUILD_BUILD_HOST=Yaroslav
 export KBUILD_BUILD_USER=t.me
 
-git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang.git -b master clang
+# git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang.git -b master clang
     
 make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all)
-
-export PATH="$HOME/vayu/clang/bin:$PATH"
+git clone -q --depth=1 https://github.com/XSans0/WeebX-Clang/releases/download/WeebX-Clang-18.1.8-release/WeebX-Clang-18.1.8.tar.gz
+tar -xvzf WeebX-Clang-18.1.8.tar.gz
+export PATH="$HOME/vayu/./bin:$PATH"
 
 make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
