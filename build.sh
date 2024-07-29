8f8a458b22c18f1e107945b3228c60b6190b7423
Yaroslav51-dev <ixityzv@gmail.com>
2024-07-29 17:44:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ export KBUILD_BUILD_USER=t.me
 # git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang.git -b master clang
     
 make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all)
-wget https://pixeldrain.com/u/1oZ32hon -P /home/runner/work/kernel_build/kernel_build/vayu/clang/
-tar -xvzf /home/runner/work/kernel_build/kernel_build/vayu/clang/WeebX-Clang-18.1.8.tar.gz
+wget https://pixeldrain.com/u/1oZ32hon -P /clang/
+unzip /home/runner/work/kernel_build/kernel_build/vayu/clang/weebx.zip
 # export PATH="/home/runner/work/kernel_build/kernel_build/vayu/clang/./bin:$PATH"
 
-make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="/home/runner/work/kernel_build/kernel_build/vayu/clang/./bin/aarch64-linux-gnu-" CROSS_COMPILE="/home/runner/work/kernel_build/kernel_build/vayu/clang/./bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/home/runner/work/kernel_build/kernel_build/vayu/clang/./bin/arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="/home/runner/work/kernel_build/kernel_build/vayu/clang/bin/aarch64-linux-gnu-" CROSS_COMPILE="/home/runner/work/kernel_build/kernel_build/vayu/clang/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/home/runner/work/kernel_build/kernel_build/vayu/clang/bin/arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
