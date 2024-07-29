55e321807125a54d2b8b70d7a7f167b82fe11f0a
Yaroslav51-dev <ixityzv@gmail.com>
2024-07-29 16:23:41 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ export KBUILD_BUILD_USER=t.me
 make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all)
 wget https://github.com/XSans0/WeebX-Clang/releases/download/WeebX-Clang-18.1.8-release/WeebX-Clang-18.1.8.tar.gz -P /home/runner/vayu/clang/
 tar -xvzf /home/runner/vayu/clang/WeebX-Clang-18.1.8.tar.gz
-# export PATH="/home/runner/vayu/clang/bin:$PATH"
+export PATH="/home/runner/work/kernel_build/kernel_build/vayu/clang/./bin:$PATH"
 
-make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="home/runner/work/kernel_build/kernel_build/vayu/clang/bin/aarch64-linux-gnu-" CROSS_COMPILE="/home/runner/work/kernel_build/kernel_build/vayu/clang/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/home/runner/work/kernel_build/kernel_build/vayu/clang/bin/arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
