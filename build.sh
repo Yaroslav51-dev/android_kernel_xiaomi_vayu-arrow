9fdd7be8d7cd312b81e1c2e3f2f4078f0c4fe262
Yaroslav51-dev <ixityzv@gmail.com>
2024-07-29 20:12:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ export KBUILD_BUILD_USER=t.me
 
 make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all)
 
-make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="$HOME/android_prebuilts_clang_host_linux-x86_clang-r510928/bin/aarch64-linux-gnu-" CROSS_COMPILE="$HOME/android_prebuilts_clang_host_linux-x86_clang-r510928/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="$HOME/android_prebuilts_clang_host_linux-x86_clang-r510928/bin/arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" CLANG_TRIPLE="${{ github.workspace }}/android_prebuilts_clang_host_linux-x86_clang-r510928/bin/aarch64-linux-gnu-" CROSS_COMPILE="${{ github.workspace }}/android_prebuilts_clang_host_linux-x86_clang-r510928/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="${{ github.workspace }}/android_prebuilts_clang_host_linux-x86_clang-r510928/bin/arm-linux-gnueabi-" LLVM=1 LLVM_IAS=1
