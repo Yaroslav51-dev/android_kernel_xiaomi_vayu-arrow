cbceba6ece90170fad8f24286e55304c5d1460fa
Yaroslav51-dev <ixityzv@gmail.com>
2024-08-02 16:34:38 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,8 @@ sudo apt-get install -y libxml2-utils libbz2-dev libbz2-1.0 libghc-bzlib-dev squ
 sudo apt-get install -y build-essential bc gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev
 sudo apt-get install -y pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl libxml-simple-perl 
 sudo apt-get install -y zip unzip tar gzip bzip2 rar unrar llvm g++-multilib bison gperf zlib1g-dev automake
-export PATH="$HOME/clang/bin:$PATH"
 export KBUILD_BUILD_HOST=Yaroslavkryt
 export KBUILD_BUILD_USER=t.me
 
-make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all)
-make -j$(nproc --all) O=out ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf HOSTCC=clang HOSTCXX=clang++ HOSTAR=llvm-ar HOSTLD=ld.lld CC="ccache clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all) SUBARCH=arm64
+make -j$(nproc --all) O=out ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 CC="ccache clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="aarch64-linux-gnu-" CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
